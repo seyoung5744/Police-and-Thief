f2ed776aed3bfabf7347f528580ad5df46b43a49
ewsnybchan <dbsqudcks123@naver.com>
2020-12-12 01:05:35 +0900

    테세우스와 미노타우로스
    자바  swing 사용

---
+++ java_final_exam/final_exam/src/final_exam/Room.java
@@ -0,0 +1,34 @@
+package final_exam;
+
+public class Room {
+	private int left, right, up, down;
+
+	public Room(int left,int right,int up,int down) {
+		this.left = left;
+		this.right = right;
+		this.up = up;
+		this.down = down;
+	}
+
+	public int getLeft() {
+		return left;
+	}
+
+	public int getRight() {
+		return right;
+	}
+
+	public int getUp() {
+		return up;
+	}
+
+	public int getDown() {
+		return down;
+	}
+
+	public void setDown(int down) {
+		this.down = down;
+	}
+	
+	
+}
